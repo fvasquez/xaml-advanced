dc9a5018da5a5693b991409960c639f22e160882
Frank Vasquez <Frank.Vasquez@gmail.com>
2016-01-16 22:21:35 -0800

    Implemented Advanced Homework Assignment 3
    Added boolean to color converter and right-tap behavior.

---
+++ RestaurantManager.Extensions/BooleanToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public sealed class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Color returnValue = Colors.Transparent;
+
+            if (value is bool)
+            {
+                returnValue = (bool)value ? Colors.Red : Colors.Transparent;
+            }
+
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool returnValue = default(bool);
+
+            if (value is Color)
+            {
+                if ((Color)value == Colors.Transparent) { returnValue = false; }
+                else if ((Color)value == Colors.Red) { returnValue = true; }
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/ThanksRightClickBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+namespace RestaurantManager.Extensions
+{
+    public sealed class ThanksRightClickBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public string Message { get; set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += OnRightTap;
+            }
+        }
+
+        public void Detach()
+        {
+            if (this.AssociatedObject != null && this.AssociatedObject is Page)
+            {
+                this.AssociatedObject = null;
+                (this.AssociatedObject as Page).RightTapped -= OnRightTap;
+            }
+        }
+
+        public void OnRightTap(object sender, RoutedEventArgs e)
+        {
+            new MessageDialog(this.Message).ShowAsync();
+        }
+    }
+}
